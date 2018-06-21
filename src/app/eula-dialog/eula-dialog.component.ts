f96e8d7fd3d6ba80fb7698264166ccedc6862672
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-20 23:55:20 -0300

    sass para scss

--- src/app/eula-dialog/eula-dialog.component.ts
+++ src/app/eula-dialog/eula-dialog.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eula-dialog',
   templateUrl: './eula-dialog.component.html',
-  styleUrls: ['./eula-dialog.component.sass']
+  styleUrls: ['./eula-dialog.component.scss']
 })
 export class EulaDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
