6d7c42f765f0ef85478a78807819bfc065ca57d1
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-20 23:55:20 -0300

    sass para scss

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to n!');
+    expect(page.getParagraphText()).toEqual('Welcome to financial-quiz-app!');
   });
 });
