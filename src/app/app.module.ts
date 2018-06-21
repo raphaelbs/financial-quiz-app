ed5a75483882d1c736ba3088066be2082696d0ff
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-20 23:42:13 -0300

    app e router desacoplados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EulaDialogComponent } from './eula-dialog/eula-dialog.component';
+import { HomeComponent } from './home/home.component';
+
+import { MatGridListModule, MatButtonModule } from '@angular/material';
+
+const MaterialDesignModules = [MatGridListModule, MatButtonModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EulaDialogComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ...MaterialDesignModules],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
