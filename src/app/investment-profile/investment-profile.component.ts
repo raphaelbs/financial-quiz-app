5ffd6e1cddbbc8d641ae934c243be19a7de44f8a
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-21 22:35:57 -0300

    perfil do investidor

---
+++ src/app/investment-profile/investment-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-profile',
+  templateUrl: './investment-profile.component.html',
+  styleUrls: ['./investment-profile.component.scss']
+})
+export class InvestmentProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
