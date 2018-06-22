794f8546b02af91749ecacbaf77af7840c4d475b
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-21 22:36:16 -0300

    ordem no module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EulaDialogComponent } from './eula-dialog/eula-dialog.component';
 import { HomeComponent } from './home/home.component';
+import { EulaDialogComponent } from './eula-dialog/eula-dialog.component';
+import { EulaResolver } from './eula-dialog/eula.resolver';
+import { EulaService } from './eula-dialog/eula.service';
+import { InvestmentProfileComponent } from './investment-profile/investment-profile.component';
 
 import {
   MatGridListModule,
@@ -14,10 +20,6 @@ import {
   MAT_DIALOG_DEFAULT_OPTIONS,
   MatCheckboxModule
 } from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
-import { EulaResolver } from './eula-dialog/eula.resolver';
-import { EulaService } from './eula-dialog/eula.service';
-import { FormsModule } from '@angular/forms';
 
 const MaterialDesignModules = [
   MatGridListModule,
@@ -27,10 +29,16 @@ const MaterialDesignModules = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, EulaDialogComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    EulaDialogComponent,
+    HomeComponent,
+    InvestmentProfileComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
+    FlexLayoutModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
