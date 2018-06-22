c837ed02cfdf8718c7ac505ee678d6ec546ddf3e
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-21 21:19:50 -0300

    httpModule, material dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EulaDialogComponent } from './eula-dialog/eula-dialog.component';
 import { HomeComponent } from './home/home.component';
 
-import { MatGridListModule, MatButtonModule } from '@angular/material';
+import {
+  MatGridListModule,
+  MatButtonModule,
+  MatDialogModule,
+  MAT_DIALOG_DEFAULT_OPTIONS
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
-const MaterialDesignModules = [MatGridListModule, MatButtonModule];
+const MaterialDesignModules = [
+  MatGridListModule,
+  MatButtonModule,
+  MatDialogModule
+];
 
 @NgModule({
   declarations: [AppComponent, EulaDialogComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, ...MaterialDesignModules],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ...MaterialDesignModules
+  ],
+  entryComponents: [EulaDialogComponent],
+  providers: [
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: true } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
