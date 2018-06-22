aaaa41b2cd82d51ea9256ce55465e01d49052fc9
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-21 23:42:58 -0300

    testes unitários no modal

--- src/app/eula-dialog/eula-dialog.component.spec.ts
+++ src/app/eula-dialog/eula-dialog.component.spec.ts
@@ -1,25 +1,71 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EulaDialogComponent } from './eula-dialog.component';
+import {
+  MatDialogModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { element } from 'protractor';
+
+export class EulaDialogMock {
+  close() {
+    return {
+      afterClosed: () => of([false])
+    };
+  }
+}
 
 describe('EulaDialogComponent', () => {
   let component: EulaDialogComponent;
   let fixture: ComponentFixture<EulaDialogComponent>;
+  let debugElement: DebugElement;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EulaDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [EulaDialogComponent],
+      imports: [
+        MatDialogModule,
+        MatButtonModule,
+        FlexLayoutModule,
+        MatCheckboxModule,
+        FormsModule
+      ],
+      providers: [
+        { provide: MatDialogRef, useClass: EulaDialogMock },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: { eula: { label: 'label', eula: 'eula' } }
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EulaDialogComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    element = debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('template should equals', () => {
+    const title = debugElement.query(By.css('h2')).nativeElement.textContent;
+    const eula = debugElement.query(By.css('mat-dialog-content')).nativeElement
+      .textContent;
+    expect(title).toEqual('label');
+    expect(eula).toEqual('eula');
   });
 });
--- src/app/eula-dialog/eula.service.spec.ts
+++ src/app/eula-dialog/eula.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { EulaService } from './eula.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('EulaService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [EulaService]
+      providers: [EulaService],
+      imports: [HttpClientModule]
     });
   });
 
