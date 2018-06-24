084f0186f36ed0e18fc20c7d67e852fc64f98550
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-24 12:46:25 -0300

    imports não utilizados removidos

--- src/app/eula-dialog/eula.resolver.ts
+++ src/app/eula-dialog/eula.resolver.ts
@@ -1,23 +1,15 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 
 import { Observable } from 'rxjs';
 import { IEula } from './eula.interface';
-import {
-  Resolve,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
-} from '@angular/router';
 import { EulaService } from './eula.service';
+import { Resolve } from '@angular/router';
 
 @Injectable()
 export class EulaResolver implements Resolve<IEula> {
   constructor(private eulaService: EulaService) {}
 
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<IEula> {
+  resolve(): Observable<IEula> {
     return this.eulaService.findEula();
   }
 }
