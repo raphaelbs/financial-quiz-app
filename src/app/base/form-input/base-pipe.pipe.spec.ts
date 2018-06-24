71ec06095b91eefdb715a804b57ae53e50a8dd94
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-06-24 20:46:58 -0300

    base pipe spec

--- src/app/base/form-input/base-pipe.pipe.spec.ts
+++ src/app/base/form-input/base-pipe.pipe.spec.ts
@@ -1,8 +1,19 @@
-import { BasePipePipe } from './base-pipe.pipe';
+import { BasePipe } from './base-pipe.pipe';
 
 describe('BasePipePipe', () => {
   it('create an instance', () => {
-    const pipe = new BasePipePipe();
+    const pipe = new BasePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform [true, true, false, true] in [true, true, true]', () => {
+    const pipe = new BasePipe();
+    expect(pipe).toBeTruthy();
+
+    const piped: Array<Boolean> = pipe.transform(
+      [true, true, false, true],
+      i => i
+    );
+    expect(piped.length).toEqual(3);
   });
 });
--- src/app/base/form-input/base-pipe.pipe.ts
+++ src/app/base/form-input/base-pipe.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'basePipe'
 })
-export class BasePipePipe implements PipeTransform {
+export class BasePipe implements PipeTransform {
   transform(items: any[], callback: (item: any) => boolean): any {
     if (!items || !callback) {
       return items;
